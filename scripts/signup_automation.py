20c3b4d12aa70a70e4429d80889b5db1c1304551
RoshanGurung-Eng <roshantheaomine@gmail.com>
2026-03-06 21:15:06 +0545

    setup account complete

--- scripts/signup_automation.py
+++ scripts/signup_automation.py
@@ -14,5 +14,18 @@ last_name.send_keys("Gurung")
 email = driver.find_element(By.NAME, "email")
 email.send_keys("roshantheaomine@gmail.com")
 
+phone_number = driver.find_element(By.NAME, "phoneNumber")
+phone_number.send_keys("9803564454")
+
+password = driver.find_element(By.NAME, "password")
+password.send_keys("98035bobU!")
+
+confirm_password = driver.find_element(By.NAME, "confirmPassword")
+confirm_password.send_keys("98035bobU!")
+
+submit_next_btn = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+submit_next_btn.click()
+
+
 input("Script complete!")
 driver.quit()
